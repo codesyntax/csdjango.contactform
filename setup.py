cadaf24f95acbb3784d6983db74268426117d1be
Urtzi Odriozola <uodriozola@codesyntax.com>
2022-10-28 11:59:23 +0200

    README format changed

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     name="csdjango.contactform",
     version=version,
     description="i18n ready contact form",
-    long_description=open("README.txt").read()
+    long_description=open("README.md").read()
     + "\n"
     + open(os.path.join("docs", "HISTORY.txt")).read(),
     # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
